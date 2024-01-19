910c571cf395dea51d25c1a06e63356608eff2c3
Aleksandr <alekcandr206@gmail.com>
2024-01-19 01:04:29 +0100

    add tests

--- src/test/java/com/samedov/springbootjwt/SpringBootJwtApplicationTests.java
+++ src/test/java/com/samedov/springbootjwt/SpringBootJwtApplicationTests.java
@@ -1,13 +1,45 @@
 package com.samedov.springbootjwt;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootJwtApplicationTests {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    void contextLoads() {
+    public void testGenerateToken() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/api/v1/token/generate", String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    public void testValidateToken() {
+        String token = restTemplate.getForObject("/api/v1/token/generate", String.class);
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/api/v1/token/validate?token=" + token, String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Token is valid", response.getBody());
+    }
+
+    @Test
+    public void testInvalidToken() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/api/v1/token/validate?token=invalidToken", String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Token is invalid", response.getBody());
     }
 
 }
